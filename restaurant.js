3ce30273317556a8736d9fa3cd0e84137168b3b6
loelam21 <42423893+loelam21@users.noreply.github.com>
2019-01-20 23:08:06 -1000

    Add files via upload

--- restaurant.js
+++ restaurant.js
@@ -3,8 +3,11 @@ xmlhttp.onreadystatechange = function() { //do this function when a certain crit
   if (this.readyState == 4 && this.status == 200) { //"this" refers to the function we are calling; 4 is ready; 200 is ok
     var mydata = JSON.parse(this.responseText); //the json file that we get will be saved as mydata
     
-    document.getElementById("nameOne").innerHTML = mydata.food[0].desc;
-    window.alert(mydata.food[0].name);
+    document.getElementById("nameOne").innerHTML = mydata.food[0].name;
+    document.getElementById("descripOne").innerHTML = mydata.food[0].desc;
+    document.getElementById("priceOne").innerHTML = "$" + mydata.food[0].price;
+    
+    window.alert(mydata.food[3].name);
   }
 };
 xmlhttp.open("GET","restaurant.json",true); //get and open the json file
