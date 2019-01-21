2b55463c33ad8d1c584e4e452b1c4e07d32df3ed
loelam21 <42423893+loelam21@users.noreply.github.com>
2019-01-20 22:43:15 -1000

    Delete restaurant.js

--- Restaurant Project (Github)/restaurant.js
+++
@@ -1,9 +0,0 @@
-var xmlhttp = new XMLHttpRequest(); //display json
-xmlhttp.onreadystatechange = function() { //do this function when a certain criteria is met
-  if (this.readyState == 4 && this.status == 200) { //"this" refers to the function we are calling; 4 is ready; 200 is ok
-    var mydata = JSON.parse(this.responseText); //the json file that we get will be saved as mydata
-    window.alert(mydata.food[3].price); //this line is how you call things
-  }
-};
-xmlhttp.open("GET","restaurant.json",true); //get and open the json file
-xmlhttp.send(); //send the file to the server
