7d3289eaa714022360719d3dd580ea4d83c7d0ad
loelam21 <42423893+loelam21@users.noreply.github.com>
2019-01-20 23:53:01 -1000

    Add files via upload

--- restaurant.js
+++ restaurant.js
@@ -1,4 +1,4 @@
-var xmlhttp = new XMLHttpRequest(); //gets content of json file
+/*var xmlhttp = new XMLHttpRequest(); //gets content of json file
 xmlhttp.onreadystatechange = function() { //do this function when a certain criteria is met
   if (this.readyState == 4 && this.status == 200) { //"this" refers to the function we are calling; 4 is ready; 200 is ok
     var mydata = JSON.parse(this.responseText); //the json file that we get will be saved as mydata
@@ -14,18 +14,22 @@ xmlhttp.onreadystatechange = function() { //do this function when a certain crit
 };
 xmlhttp.open("GET","restaurant.json",true); //get and open the json file
 xmlhttp.send(); //send the file to the server
-
+*/
 //Display running total of cost-----------------------------------------------------------------------------------------------------------------------------------------------------
 var runningTotal = 0;
 
 function orderOne() {
   var amount = document.getElementById('inputOne').value;
-  var basePrice = document.getElementById('priceOne').innerHTML;
+  //var basePrice = document.getElementById('priceOne').innerHTML;
   if (amount >= 0) {
-    var orderThis = amount * basePrice;
-    document.getElementById('subtotal').innerHTML = orderThis;
+    //var orderThis = amount * basePrice;
+    var orderThis = amount * 2.00;
+    var newRunningTotal = runningTotal + orderThis;
+    document.getElementById('subtotal').innerHTML = newRunningTotal;
+    amount = "";
   }
   else {
     window.alert("Error. The entered value must be positive whole number.");
+    amount = "";
   }
 }
