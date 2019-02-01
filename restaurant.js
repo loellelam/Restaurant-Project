c65d8cd5041437fc1dc8695ef28c8f3390b7c681
loelam21 <42423893+loelam21@users.noreply.github.com>
2019-01-31 14:40:28 -1000

    Add files via upload

--- restaurant.js
+++ restaurant.js
@@ -4,23 +4,22 @@ xmlhttp.onreadystatechange = function() { //do this function when a certain crit
     var mydata = JSON.parse(this.responseText); //the json file that we get will be saved as mydata
     
     document.getElementById('nameOne').innerHTML = mydata.dish[0].name;
-    //document.getElementById('descripOne').innerHTML = mydata.dish[0].desc;
     document.getElementById('priceOne').innerHTML = mydata.dish[0].price;
     
     document.getElementById('nameTwo').innerHTML = mydata.dish[1].name;
-    //document.getElementById('descripTwo').innerHTML = mydata.dish[1].desc;
     document.getElementById('priceTwo').innerHTML = mydata.dish[1].price;
-
+  
     document.getElementById('nameThree').innerHTML = mydata.dish[2].name;
     document.getElementById('priceThree').innerHTML = mydata.dish[2].price;
-
+  
     document.getElementById('nameFour').innerHTML = mydata.dish[3].name;
     document.getElementById('priceFour').innerHTML = mydata.dish[3].price;
-
+  
     document.getElementById('nameFive').innerHTML = mydata.dish[4].name;
     document.getElementById('priceFive').innerHTML = mydata.dish[4].price;
-
+  
     document.getElementById('nameSix').innerHTML = mydata.dish[5].name;
+    //document.getElementById('descripOne').innerHTML = mydata.dish[5].desc;
     document.getElementById('priceSix').innerHTML = mydata.dish[5].price;
   }
 };
@@ -53,91 +52,209 @@ var amountThree = 0;
 var amountFour = 0;
 var amountFive = 0;
 var amountSix = 0;
-//Check keypresses-----------------------------------------------------------------------------------------------------------------------------------------------------
-/*inputOne.onkeypress = function() {
-  if (event.keyCode == 46) { //46 is decimal
-    document.getElementById('inputOne').value = ""; //clear input box
-    window.alert("No decimals!");
-  }
-}*/
 
 //Calculate and display costs-----------------------------------------------------------------------------------------------------------------------------------------------------
 function dishOne() {
   var qty = document.getElementById('selOne').value; //takes user input
-  //var qty = document.getElementById('inputOne').value; //takes user input
-  //document.getElementById('inputOne').value = ""; //clear input box
   var basePrice = document.getElementById('priceOne').innerHTML; //get price of item
   
-  if (qty > 0) { //verify user's input
-    localStorage.setItem("boughtOne", true); //tells me user bought this dish
-    window.alert("num 1 work");
-    //save user's qty to local storage
-    var addMe = +qty + qtyOfZero;
-    window.alert("num 2 work");
-    qtyOfZero = addMe;
-    localStorage.setItem("storeQtyOfOne", qtyOfZero);
+  localStorage.setItem("boughtOne", true); //tells me user bought this dish
+  
+  //save user's qty to local storage
+  var addMe = +qty + qtyOfZero;
+  qtyOfZero = addMe;
+  localStorage.setItem("storeQtyOfOne", qtyOfZero);
 
-    //subtotal
-    var orderThis = qty * basePrice; //cost of ordered item
-        //save amount to localStorage
-        var addMeHere = +orderThis + amountOne;
-        amountOne = addMeHere;
-        localStorage.setItem("amountOne", amountOne);
-    var getRunningTotal = Number(localStorage.getItem("runningTotal")); //get runningTotal from localStorage
-    var newRunningTotal = (getRunningTotal + orderThis).toFixed(2); //calculate subtotal
-    localStorage.setItem("runningTotal", newRunningTotal); //save subtotal into localStorage
-    document.getElementById('showSubtotal').innerHTML = newRunningTotal; //display subtotal
-    
-    //tax
-    var calcTax = (newRunningTotal * 0.04712).toFixed(2); //find pure tax
-    localStorage.setItem("tax", calcTax); //save tax to localStorage
-    document.getElementById('showTax').innerHTML = calcTax; //display tax
-    
-    //total
-    var newTotal = (Number(newRunningTotal) + Number(calcTax)).toFixed(2); //calculate total
-    localStorage.setItem("total", newTotal); //save total to localStorage
-    document.getElementById('showTotal').innerHTML = newTotal; //display total
-  }
-  else {
-    window.alert("Error. Value must be a positive whole number.");
-  }
+  //subtotal
+  var orderThis = qty * basePrice; //cost of ordered item
+      //save amount to localStorage
+      var addMeHere = +orderThis + amountOne;
+      amountOne = addMeHere;
+      localStorage.setItem("amountOne", amountOne);
+  var getRunningTotal = Number(localStorage.getItem("runningTotal")); //get runningTotal from localStorage
+  var newRunningTotal = (getRunningTotal + orderThis).toFixed(2); //calculate subtotal
+  localStorage.setItem("runningTotal", newRunningTotal); //save subtotal into localStorage
+  document.getElementById('showSubtotal').innerHTML = newRunningTotal; //display subtotal
+  
+  //tax
+  var calcTax = (newRunningTotal * 0.04712).toFixed(2); //find pure tax
+  localStorage.setItem("tax", calcTax); //save tax to localStorage
+  document.getElementById('showTax').innerHTML = calcTax; //display tax
+  
+  //total
+  var newTotal = (Number(newRunningTotal) + Number(calcTax)).toFixed(2); //calculate total
+  localStorage.setItem("total", newTotal); //save total to localStorage
+  document.getElementById('showTotal').innerHTML = newTotal; //display total
 }
 
 function dishTwo() {
-  localStorage.setItem("boughtTwo", true); //tells me user bought this dish
-
-  var qty = document.getElementById('inputTwo').value; //takes user input
-  document.getElementById('inputTwo').value = ""; //clear input box
+  var qty = document.getElementById('selTwo').value; //takes user input
   var basePrice = document.getElementById('priceTwo').innerHTML; //get price of item
-
-  //save user's qty to local storage
-  var addMe = +qty + qtyOfTwo;
-  localStorage.setItem("storeQtyOf1", addMe);
-  //save total price of item to localStorage
   
-  if (qty >= 0) { //verify user's input
-    //subtotal
-    var orderThis = qty * basePrice; //cost of ordered item
-    //var orderThis = qty * 1; //cost of ordered item
-    var getRunningTotal = Number(localStorage.getItem("runningTotal")); //get runningTotal from localStorage
-    var newRunningTotal = (getRunningTotal + orderThis).toFixed(2); //calculate subtotal
-    localStorage.setItem("runningTotal", newRunningTotal); //save subtotal into localStorage
-    document.getElementById('showSubtotal').innerHTML = newRunningTotal; //display subtotal
-    
-    //tax
-    var calcTax = (newRunningTotal * 0.04712).toFixed(2); //find pure tax
-    localStorage.setItem("tax", calcTax); //save tax to localStorage
-    document.getElementById('showTax').innerHTML = calcTax; //display tax
-    
-    //total
-    var newTotal = (Number(newRunningTotal) + Number(calcTax)).toFixed(2); //calculate total
-    localStorage.setItem("total", newTotal); //save total to localStorage
-    document.getElementById('showTotal').innerHTML = newTotal; //display total
-  }
-  else {
-    window.alert("Error. Value must be a positive whole number.");
-  }
+  localStorage.setItem("boughtTwo", true); //tells me user bought this dish
+  
+  //save user's qty to local storage
+  var addMe = +qty + qtyOfZero;
+  qtyOfZero = addMe;
+  localStorage.setItem("storeQtyOfTwo", qtyOfZero);
+
+  //subtotal
+  var orderThis = qty * basePrice; //cost of ordered item
+      //save amount to localStorage
+      var addMeHere = +orderThis + amountTwo;
+      amountTwo = addMeHere;
+      localStorage.setItem("amountTwo", amountTwo);
+  var getRunningTotal = Number(localStorage.getItem("runningTotal")); //get runningTotal from localStorage
+  var newRunningTotal = (getRunningTotal + orderThis).toFixed(2); //calculate subtotal
+  localStorage.setItem("runningTotal", newRunningTotal); //save subtotal into localStorage
+  document.getElementById('showSubtotal').innerHTML = newRunningTotal; //display subtotal
+  
+  //tax
+  var calcTax = (newRunningTotal * 0.04712).toFixed(2); //find pure tax
+  localStorage.setItem("tax", calcTax); //save tax to localStorage
+  document.getElementById('showTax').innerHTML = calcTax; //display tax
+  
+  //total
+  var newTotal = (Number(newRunningTotal) + Number(calcTax)).toFixed(2); //calculate total
+  localStorage.setItem("total", newTotal); //save total to localStorage
+  document.getElementById('showTotal').innerHTML = newTotal; //display total
 }
+
+function dishThree() {
+  var qty = document.getElementById('selThree').value; //takes user input
+  var basePrice = document.getElementById('priceThree').innerHTML; //get price of item
+  
+  localStorage.setItem("boughtThree", true); //tells me user bought this dish
+  
+  //save user's qty to local storage
+  var addMe = +qty + qtyOfZero;
+  qtyOfZero = addMe;
+  localStorage.setItem("storeQtyOfThree", qtyOfZero);
+
+  //subtotal
+  var orderThis = qty * basePrice; //cost of ordered item
+      //save amount to localStorage
+      var addMeHere = +orderThis + amountThree;
+      amountThree = addMeHere;
+      localStorage.setItem("amountThree", amountThree);
+  var getRunningTotal = Number(localStorage.getItem("runningTotal")); //get runningTotal from localStorage
+  var newRunningTotal = (getRunningTotal + orderThis).toFixed(2); //calculate subtotal
+  localStorage.setItem("runningTotal", newRunningTotal); //save subtotal into localStorage
+  document.getElementById('showSubtotal').innerHTML = newRunningTotal; //display subtotal
+  
+  //tax
+  var calcTax = (newRunningTotal * 0.04712).toFixed(2); //find pure tax
+  localStorage.setItem("tax", calcTax); //save tax to localStorage
+  document.getElementById('showTax').innerHTML = calcTax; //display tax
+  
+  //total
+  var newTotal = (Number(newRunningTotal) + Number(calcTax)).toFixed(2); //calculate total
+  localStorage.setItem("total", newTotal); //save total to localStorage
+  document.getElementById('showTotal').innerHTML = newTotal; //display total
+}
+
+function dishFour() {
+  var qty = document.getElementById('selFour').value; //takes user input
+  var basePrice = document.getElementById('priceFour').innerHTML; //get price of item
+  
+  localStorage.setItem("boughtFour", true); //tells me user bought this dish
+  
+  //save user's qty to local storage
+  var addMe = +qty + qtyOfZero;
+  qtyOfZero = addMe;
+  localStorage.setItem("storeQtyOfFour", qtyOfZero);
+
+  //subtotal
+  var orderThis = qty * basePrice; //cost of ordered item
+      //save amount to localStorage
+      var addMeHere = +orderThis + amountFour;
+      amountFour = addMeHere;
+      localStorage.setItem("amountFour", amountFour);
+  var getRunningTotal = Number(localStorage.getItem("runningTotal")); //get runningTotal from localStorage
+  var newRunningTotal = (getRunningTotal + orderThis).toFixed(2); //calculate subtotal
+  localStorage.setItem("runningTotal", newRunningTotal); //save subtotal into localStorage
+  document.getElementById('showSubtotal').innerHTML = newRunningTotal; //display subtotal
+  
+  //tax
+  var calcTax = (newRunningTotal * 0.04712).toFixed(2); //find pure tax
+  localStorage.setItem("tax", calcTax); //save tax to localStorage
+  document.getElementById('showTax').innerHTML = calcTax; //display tax
+  
+  //total
+  var newTotal = (Number(newRunningTotal) + Number(calcTax)).toFixed(2); //calculate total
+  localStorage.setItem("total", newTotal); //save total to localStorage
+  document.getElementById('showTotal').innerHTML = newTotal; //display total
+}
+
+function dishFive() {
+  var qty = document.getElementById('selFive').value; //takes user input
+  var basePrice = document.getElementById('priceFive').innerHTML; //get price of item
+  
+  localStorage.setItem("boughtFive", true); //tells me user bought this dish
+  
+  //save user's qty to local storage
+  var addMe = +qty + qtyOfZero;
+  qtyOfZero = addMe;
+  localStorage.setItem("storeQtyOfFive", qtyOfZero);
+
+  //subtotal
+  var orderThis = qty * basePrice; //cost of ordered item
+      //save amount to localStorage
+      var addMeHere = +orderThis + amountFive;
+      amountFive = addMeHere;
+      localStorage.setItem("amountFive", amountFive);
+  var getRunningTotal = Number(localStorage.getItem("runningTotal")); //get runningTotal from localStorage
+  var newRunningTotal = (getRunningTotal + orderThis).toFixed(2); //calculate subtotal
+  localStorage.setItem("runningTotal", newRunningTotal); //save subtotal into localStorage
+  document.getElementById('showSubtotal').innerHTML = newRunningTotal; //display subtotal
+  
+  //tax
+  var calcTax = (newRunningTotal * 0.04712).toFixed(2); //find pure tax
+  localStorage.setItem("tax", calcTax); //save tax to localStorage
+  document.getElementById('showTax').innerHTML = calcTax; //display tax
+  
+  //total
+  var newTotal = (Number(newRunningTotal) + Number(calcTax)).toFixed(2); //calculate total
+  localStorage.setItem("total", newTotal); //save total to localStorage
+  document.getElementById('showTotal').innerHTML = newTotal; //display total
+}
+
+function dishSix() {
+  var qty = document.getElementById('selSix').value; //takes user input
+  //var qty = document.getElementById('inputSix').value; //takes user input
+  //document.getElementById('inputSix').value = ""; //clear input box
+  var basePrice = document.getElementById('priceSix').innerHTML; //get price of item
+  
+  localStorage.setItem("boughtSix", true); //tells me user bought this dish
+  
+  //save user's qty to local storage
+  var addMe = +qty + qtyOfZero;
+  qtyOfZero = addMe;
+  localStorage.setItem("storeQtyOfSix", qtyOfZero);
+
+  //subtotal
+  var orderThis = qty * basePrice; //cost of ordered item
+      //save amount to localStorage
+      var addMeHere = +orderThis + amountSix;
+      amountSix = addMeHere;
+      localStorage.setItem("amountSix", amountSix);
+  var getRunningTotal = Number(localStorage.getItem("runningTotal")); //get runningTotal from localStorage
+  var newRunningTotal = (getRunningTotal + orderThis).toFixed(2); //calculate subtotal
+  localStorage.setItem("runningTotal", newRunningTotal); //save subtotal into localStorage
+  document.getElementById('showSubtotal').innerHTML = newRunningTotal; //display subtotal
+  
+  //tax
+  var calcTax = (newRunningTotal * 0.04712).toFixed(2); //find pure tax
+  localStorage.setItem("tax", calcTax); //save tax to localStorage
+  document.getElementById('showTax').innerHTML = calcTax; //display tax
+  
+  //total
+  var newTotal = (Number(newRunningTotal) + Number(calcTax)).toFixed(2); //calculate total
+  localStorage.setItem("total", newTotal); //save total to localStorage
+  document.getElementById('showTotal').innerHTML = newTotal; //display total
+}
+
+
 
 //Proceed to checkout-----------------------------------------------------------------------------------------------------------------------------------------------------------
 function checkout() {
